9604b096615c592b41e8db087ce3a95114b251da
LukasDragseth <157658147+LukasDragseth@users.noreply.github.com>
2025-09-29 11:08:59 -0400

    Update MathPractice2.java

--- src/MathPractice2.java
+++ src/MathPractice2.java
@@ -61,7 +61,6 @@ public class MathPractice2 {
 
     // 10. Return the absolute difference between two integers.
     public static int q10(int a, int b) {
-        // TODO: Implement
-        return 0;
+        
     }
 }
